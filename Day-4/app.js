a3f4f75cede72b63ba1040cecc601248979ef4da
Pg Gonni <p.gonni@hotmail.com>
2020-06-12 10:38:04 -0400

    added first loop

--- Day-4/app.js
+++ Day-4/app.js
@@ -1,3 +1,12 @@
 function sumAllPrimes(num) {
-  
-}
+  // Define empty array
+  let arr = [];
+  // for loop while i<=num
+  for (let i = 1; i <= num; i++) {
+    // add i to array
+    arr.push(i);
+  };
+  console.log(arr);
+};
+
+const challenge = sumAllPrimes(10);
