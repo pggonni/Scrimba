d84311baba50e59db1ea346b1e53621cf0ba8c1d
Pg Gonni <p.gonni@hotmail.com>
2020-06-12 13:25:48 -0400

    added prime checker

--- Day-4/app.js
+++ Day-4/app.js
@@ -1,12 +1,30 @@
 function sumAllPrimes(num) {
-  // Define empty array
+  //Define empty array
   let arr = [];
-  // for loop while i<=num
+  //for loop while i<=num
   for (let i = 1; i <= num; i++) {
-    // add i to array
+    //add i to array
     arr.push(i);
   };
   console.log(arr);
+  //Define empty array to hold prime numbers
+  let primes = [];
+  //for of loop over array
+  for (let n of arr) {
+    for (let i = 2; i < n; i++) {
+      if (n % i === 0) {
+        break;
+      } else {
+        primes.push(n);
+        break;
+      };
+    };
+  };
+  console.log(primes);
+  let result = primes.reduce((accumulator, currVal) => {
+    return accumulator + currVal
+  }, 0);
+  return result
 };
 
-const challenge = sumAllPrimes(10);
+sumAllPrimes(10);
