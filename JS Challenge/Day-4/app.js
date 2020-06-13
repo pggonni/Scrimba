cef066a4f221c166aa54fa4314c31d00f024d844
Pg Gonni <p.gonni@hotmail.com>
2020-06-13 15:53:31 -0400

    Gave up on Day 4
    Just couldn't get my answer to work properly. It was mostly fine except for a weird quirk where 9 kept getting added as a prime.

--- JS Challenge/Day-4/app.js
+++ JS Challenge/Day-4/app.js
@@ -1,30 +1,49 @@
+// function sumAllPrimes(num) {
+//   //Define empty array
+//   let arr = [];
+//   //for loop while i<=num
+//   for (let i = 1; i <= num; i++) {
+//     //add i to array
+//     arr.push(i);
+//   };
+//   console.log(arr);
+//   //Define empty array to hold prime numbers
+//   let primes = [];
+//   //for of loop over array
+//   for (let n of arr) {
+//     for (let i = 2; i < n; i++) {
+//       if (n % i != 0) {
+//         primes.push(n);
+//         break;
+//       } else {
+//         break;
+//       };
+//     };
+//   };
+//   console.log(primes);
+//   let result = primes.reduce((accumulator, currVal) => {
+//     return accumulator + currVal
+//   }, 0);
+//   console.log(result);
+// };
+
+//*********************************************************************
+// SOLUTION         
+//*********************************************************************
+
 function sumAllPrimes(num) {
-  //Define empty array
-  let arr = [];
-  //for loop while i<=num
-  for (let i = 1; i <= num; i++) {
-    //add i to array
-    arr.push(i);
-  };
-  console.log(arr);
-  //Define empty array to hold prime numbers
-  let primes = [];
-  //for of loop over array
-  for (let n of arr) {
-    for (let i = 2; i < n; i++) {
-      if (n % i === 0) {
-        break;
-      } else {
-        primes.push(n);
+  let primesTotal = 0;
+
+  for (let i = 2; i < num; i++) {
+    for (let j = 2; j <= i; j++) {
+      if (i === j) {
+        primesTotal += i;
+      };
+
+      if (i % j === 0) {
         break;
       };
     };
   };
-  console.log(primes);
-  let result = primes.reduce((accumulator, currVal) => {
-    return accumulator + currVal
-  }, 0);
-  return result
+  return primesTotal;
 };
-
-sumAllPrimes(10);
